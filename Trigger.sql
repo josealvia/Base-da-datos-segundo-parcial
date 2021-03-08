9ea6be62e3e0c9c1dbe9b373aafb1aaa6b695fdc
josealvia <70083017+josealvia@users.noreply.github.com>
2021-03-08 15:43:03 -0500

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -1,1 +1,19 @@
-
+create or replace TRIGGER TR_AUMENTO
+before
+insert on venta
+for each row
+declare
+    cursor c_aumento is select vendedor.nombres_vendedor, 
+    count(venta.descuento_venta) as total_desc from venta, vendedor 
+    where venta.codigo_vendedor = vendedor.codigo_vendedor
+    group by vendedor.nombres_vendedor;
+    
+begin
+    for registro in c_aumento
+    loop
+    if(registro.total_desc > 3 ) then
+        update vendedor set sueldo_mensual = sueldo_mensual + 100 where codigo_vendedor =:new.codigo_vendedor;
+        DBMS_OUTPUT.PUT_LINE('Has recibido un aumento');
+        end if;
+    end loop;
+end TR_AUMENTO;
