d4ee83a8e558048e4e60278f611916ade3cd0c38
josealvia <70083017+josealvia@users.noreply.github.com>
2021-03-08 15:54:29 -0500

    Update ireport.sql

--- ireport.sql
+++ ireport.sql
@@ -1,1 +1,4 @@
-
+SELECT sucursal.direccion_sucursal, count(id_venta) as Total_ventas FROM VENTA
+inner join vendedor on venta.codigo_vendedor=vendedor.codigo_vendedor
+inner join sucursal on vendedor.codigo_sucursal= sucursal.codigo_sucursal
+group by sucursal.direccion_sucursal
