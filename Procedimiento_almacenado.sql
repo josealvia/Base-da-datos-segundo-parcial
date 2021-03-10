1e3e5692c172013ca6a21087215b682d21686c9c
josealvia <70083017+josealvia@users.noreply.github.com>
2021-03-09 20:31:25 -0500

    Update Procedimiento_almacenado.sql

--- Procedimiento_almacenado.sql
+++ Procedimiento_almacenado.sql
@@ -1,1 +1,20 @@
+create or replace procedure mostrarsubalterno(n_vendedor vendedor.codigo_vendedor%type)
+as
+cursor subalterno is 
+select s.nombre_subalterno, s.apellido_sualterno
+from vendedor v, subaltero s
+where v.codigo_vendedor = s.codigo_vendedor
+and v.codigo_vendedor=n_vendedor;
+begin 
+    for registro in subalterno
+    loop
+        dbms_output.put_line('Nombre: '||registro.nombre_subalterno || 'Apellido: '||registro.apellido_sualterno);
+    
+    end loop;
+end;
 
+declare 
+    n_vendedor vendedor.codigo_vendedor%type := &codigo;
+begin
+    mostrarsubalterno(n_vendedor);
+end;
